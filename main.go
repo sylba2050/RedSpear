04bb7581f7dec7fd03a9ea5a0d07506243b91a7c
Masataka Hisasue <masataka.hisasue@optim.co.jp>
2019-03-23 06:50:48 +0900

    feat: add viewer

--- main.go
+++ main.go
@@ -14,6 +14,14 @@ func main() {
     e.Use(middleware.Logger())
     e.Use(middleware.CORS())
 
+    e.File("/", "html/index.html")
+
+    e.File("/js/main.js", "js/main.js")
+
+    e.File("/css/style.css", "css/style.css")
+    e.File("/css/code.css", "css/code.css")
+    e.File("/css/github.css", "css/github.css")
+
     e.POST("api/md", markdown.Default)
 
     e.Start(":8080")
