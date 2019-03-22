c4b3e5862a170d0e78835ae6d392386f6f744e51
Masataka Hisasue <masataka.hisasue@optim.co.jp>
2019-03-23 07:38:21 +0900

    style: rename write.html and write.js

--- main.go
+++ main.go
@@ -14,9 +14,9 @@ func main() {
     e.Use(middleware.Logger())
     e.Use(middleware.CORS())
 
-    e.File("/", "html/index.html")
+    e.File("/write", "html/write.html")
 
-    e.File("/js/main.js", "js/main.js")
+    e.File("/js/write.js", "js/write.js")
 
     e.File("/css/style.css", "css/style.css")
     e.File("/css/code.css", "css/code.css")
