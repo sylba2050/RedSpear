c84ac8cf77a6f165f6752b4de2c17d11cb5a7f59
Masataka Hisasue <masataka.hisasue@optim.co.jp>
2019-05-26 17:00:52 +0900

    style: formated by prettier

--- js/index.js
+++ js/index.js
@@ -1,40 +1,44 @@
-window.onload = function () {
+window.onload = function() {
   var main = new Vue({
-    el: '#main',
+    el: "#main",
     data: {
       articles: [],
       article_detail_data: {},
       is_detail: false,
-      html: "",
+      html: ""
     },
-    beforeCreate: function(){
+    beforeCreate: function() {
       fetch("/articles", {
-          method: 'GET',
-          mode: 'cors',
-      }).then((response) => {
-          return response.json()
-      }).then((json) => {
+        method: "GET",
+        mode: "cors"
+      })
+        .then(response => {
+          return response.json();
+        })
+        .then(json => {
           this.articles = json;
-      });
+        });
     },
     watch: {
-      article_detail_data: function(){
+      article_detail_data: function() {
         var request_json = {};
         request_json.md = this.article_detail_data.content;
         console.log(request_json);
 
         fetch("/md", {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(request_json)
-        }).then((response) => {
-            return response.text()
-        }).then((text) => {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(request_json)
+        })
+          .then(response => {
+            return response.text();
+          })
+          .then(text => {
             this.html = text;
-        });
+          });
       }
     },
     methods: {
@@ -42,15 +46,17 @@ window.onload = function () {
         this.is_detail = true;
 
         fetch("/article/" + id, {
-            method: 'GET',
-            mode: 'cors',
-        }).then((response) => {
-            return response.json()
-        }).then((json) => {
+          method: "GET",
+          mode: "cors"
+        })
+          .then(response => {
+            return response.json();
+          })
+          .then(json => {
             this.article_detail_data = json;
             console.log(this.article_detail_data);
-        });
+          });
       }
     }
-  })
-}
+  });
+};
--- js/write.js
+++ js/write.js
@@ -1,6 +1,6 @@
-window.onload = function () {
+window.onload = function() {
   var app = new Vue({
-    el: '#main',
+    el: "#main",
     data: {
       title: "",
       markdown: "",
@@ -8,66 +8,72 @@ window.onload = function () {
       hoge: false,
       markdownLastChangeCount: 0,
       markdown2htmlConvertFlg: false,
-      count: 0,
+      count: 0
     },
-    created: function(){
-        setInterval(() => { this.markdownLastChangeCount++ }, 1)
-        setInterval(() => { this.count++ }, 100)
+    created: function() {
+      setInterval(() => {
+        this.markdownLastChangeCount++;
+      }, 1);
+      setInterval(() => {
+        this.count++;
+      }, 100);
     },
     watch: {
-        markdown: function(){
-            this.markdownLastChangeCount = 0;
-            this.markdown2htmlConvertFlg = false;
-        },
-        count: function(){
-            if (this.markdownLastChangeCount < 100){
-                return;
-            }
-            if (this.markdown2htmlConvertFlg){
-                return;
-            }
-            this.markdown2htmlConvertFlg = true;
+      markdown: function() {
+        this.markdownLastChangeCount = 0;
+        this.markdown2htmlConvertFlg = false;
+      },
+      count: function() {
+        if (this.markdownLastChangeCount < 100) {
+          return;
+        }
+        if (this.markdown2htmlConvertFlg) {
+          return;
+        }
+        this.markdown2htmlConvertFlg = true;
 
-            var request_json = {};
-            request_json.md = this.markdown;
+        var request_json = {};
+        request_json.md = this.markdown;
 
-            fetch("/md", {
-                method: 'POST',
-                mode: 'cors',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(request_json)
-            }).then((response) => {
-                return response.text()
-            }).then((text) => {
-                app.html = text;
-            });
-        },
+        fetch("/md", {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(request_json)
+        })
+          .then(response => {
+            return response.text();
+          })
+          .then(text => {
+            app.html = text;
+          });
+      }
     },
     methods: {
-        postArticle : function() {
-            var article = {};
-            article.userid = 'sylba2050';
-            article.title = this.title;
-            article.content = this.markdown;
-            article.cp = 0;
+      postArticle: function() {
+        var article = {};
+        article.userid = "sylba2050";
+        article.title = this.title;
+        article.content = this.markdown;
+        article.cp = 0;
 
-            fetch("/article", {
-                method: 'POST',
-                mode: 'cors',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(article)
-            }).then(response => {
-                if (response.ok) {
-                    console.log("success");
-                } else {
-                    console.log("failed");
-                }
-            });
-        }
+        fetch("/article", {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(article)
+        }).then(response => {
+          if (response.ok) {
+            location.href = "/";
+          } else {
+            console.log("failed");
+          }
+        });
+      }
     }
-  })
-}
+  });
+};
